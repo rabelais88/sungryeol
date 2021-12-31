0ea319b81af2aee4fbc65019d6c6d9a2ed44902c
sungryeol park <sungryeolp@gmail.com>
2021-12-31 20:02:03 +0900

    fixing package deploying issues

--- app-portfolio-2021/next.config.js
+++ app-portfolio-2021/next.config.js
@@ -1,20 +1,5 @@
-const { config } = require('process');
-
-const withNextTranspiler = require('next-transpile-modules')([
-  '@sungryeol/lib',
-]);
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
   outputFileTracing: false,
-  webpack: (config, options) => {
-    config.resolve.alias = {
-      ...config.resolve.alias,
-      '@sungryeol/lib': require.resolve('@sungryeol/lib'),
-    };
-    return config;
-  },
 };
-
-module.exports = withNextTranspiler(nextConfig);
