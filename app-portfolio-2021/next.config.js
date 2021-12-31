fa8207f52e263a95007db397231185c3193383e3
sungryeol park <sungryeolp@gmail.com>
2021-12-31 18:41:27 +0900

    fixing vercel deploying issues with nextjs webpack transpiler

--- app-portfolio-2021/next.config.js
+++ app-portfolio-2021/next.config.js
@@ -1,5 +1,19 @@
+const { config } = require('process');
+
+const withNextTranspiler = require('next-transpile-modules')([
+  '@sungryeol/lib',
+]);
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+const nextConfig = {
   reactStrictMode: true,
   outputFileTracing: false,
+  webpack: (config, options) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@sungryeol/lib': require.resolve('@sungryeol/lib'),
+    };
+  },
 };
+
+module.exports = withNextTranspiler(nextConfig);
